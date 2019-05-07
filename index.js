77be10d7c976ab9fb04467c2b3f4f19ba21fd0db
Paris Dolfman <parisdolfman@gmail.com>
2019-05-07 00:11:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 
 function binaryMatch(magazine) {
-  for (let i = 0; i < magazine.length; i++) {
-    magazine[i] // access each element in array
-
+  let result = []
+  array = magazine.sort();
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] == array[j]) {
+        // only add to result array once
+        }
+      }
+    }
   }
+}
+
+function getFrequency(array) {
+  var frequency = {};
+  for (let i = 0; i < array.length; i++) {
+    var character = array.charAt(i);
+    if (frequency[character]) {
+      frequency[character]++;
+    } else {
+      frequency[character] = 1;
+    }
+  }
+  return frequency
 }
